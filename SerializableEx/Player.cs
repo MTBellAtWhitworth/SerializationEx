5815308f06ce2e051305503b0ad97d65c1d6ac31
Dr. Matthew Bell <mbell@whitworth.edu>
2024-01-19 10:02:32 -0800

    Initial commit

---
+++ SerializableEx/Player.cs
@@ -0,0 +1,36 @@
+﻿////////////////////////////////////////////////////
+///A simple example designed by M.Bell to use Troelsen and Japikse, 2022
+///to show off serialization in .NET. As ever, Dr. Bell
+///loves RPGs.
+///////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializableEx
+{
+    [Serializable]
+    public class Player
+    {
+        public int Location { get; set; } // Holds the location for a player
+        public string Name { get; set; }  // Holds a player name
+        public int HP { get; set; }       // Their hit points
+        //Other attibutes can go here....think big
+
+        public Player() : this("Joe Runner") {} //<= Must have a default constructor for this to work!
+
+        public Player(string name) : this(name, 1) {}
+
+        public Player(string name, int hits) : this(name, hits, 0) {}
+
+        public Player(string name, int hits, int loc)
+        {
+            Location = loc;
+            Name = name;
+            HP = hits;
+        }
+    }
+}
---
+++ SerializableEx/Program.cs
@@ -0,0 +1,34 @@
+﻿///////////////////////////////////////////////
+///Example crafted by M.Bell using Troelsen and Japikse, 2022
+///Demonstrates XML serialization
+///Originally developed for CS371, Jan 2019
+///Updated for .NET 8 / C# 6 in Jan 2024
+//////////////////////////////////////////////
+
+using SerializableEx;
+using System.Xml.Serialization; //<= Hafta add this line
+
+namespace SaveGameState
+{
+    class Program
+    {
+        //This function inspired by and largely taken from T&J, pp. 781ff
+        static void SaveAsXmlFormat<T>(T entity, string filename)
+        {
+            // Save entity to filename as a binary file, see p. 789 in T&J
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(T));
+            using (Stream fStream =
+                new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlFormat.Serialize(fStream, entity);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Make a player:
+            Player myMazeRunner = new Player("Thomas", 10);
+            SaveAsXmlFormat(myMazeRunner, "Player.xml");
+        }
+    }
+}
